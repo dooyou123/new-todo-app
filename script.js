8f2b171e3bd979862a4ff511b0de1a9f5cda6fbc
dooyou123 <112735161+dooyou123@users.noreply.github.com>
2023-06-30 15:03:04 +0900

    123

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const list = document.getElementById("todo-list");
 
 const CLIENT_ID =
   "171103722680-b91r63bg0o6bhmtd3vd62ti5ki802ppe.apps.googleusercontent.com";
-const API_KEY = "AIzaSyBNnUzoqs3GJDCy_LNIWpSvni3XWX1nneU";
+const API_KEY = "YOUR_API_KEY";
 const SCOPE = "https://www.googleapis.com/auth/spreadsheets";
 
 const SPREADSHEET_ID = "18k1pDmwY99ovCVJRhHF_pOIViyLZPKp5yxK1QfspizQ";
@@ -15,6 +15,7 @@ form.addEventListener("submit", function (event) {
   if (task) {
     addTask(task);
     input.value = "";
+    setTodoListToSheet(task);
   }
 });
 
@@ -31,63 +32,58 @@ function addTask(task) {
   list.appendChild(listItem);
 }
 
-gapi.load("client:auth2", initClient);
-
 function initClient() {
-    // Google API 클라이언트 초기화
-    gapi.client
-      .init({
-        apiKey: "<AIzaSyBNnUzoqs3GJDCy_LNIWpSvni3XWX1nneU>",
-        clientId: "<171103722680-b91r63bg0o6bhmtd3vd62ti5ki802ppe.apps.googleusercontent.com>",
-        discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/sheets/v4/rest"],
-        scope: "https://www.googleapis.com/auth/spreadsheets",
-      })
-      .then(() => {
-        // 초기화가 성공하면 로그인/로그아웃 버튼 상태를 업데이트할 수 있습니다.
-        updateLoginStatus();
-      })
-      .catch(() => {
-        console.log("Error initializing Google client Object");
-      });
-  }
-  
-  // updateLoginStatus() 함수는 사용자의 로그인 상태를 확인하고, 로그인/로그아웃 버튼 상태를 업데이트하는 기능을 구현합니다.
+  // Google API 클라이언트 초기화
+  gapi.client
+    .init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/sheets/v4/rest"],
+      scope: SCOPE,
+    })
+    .then(() => {
+      // 초기화가 성공하면 로그인/로그아웃 버튼 상태를 업데이트할 수 있습니다.
+      updateLoginStatus();
+    })
+    .catch(() => {
+      console.log("Error initializing Google client Object");
+    });
+}
+
 function updateLoginStatus() {
-    const authInstance = gapi.auth2.getAuthInstance();
-    const isSignedIn = authInstance.isSignedIn.get();
-  
-    if (isSignedIn) {
-      // 로그인된 상태라면 로그아웃 버튼을 보여줍니다.
-      document.getElementById("loginBtn").style.display = "none";
-      document.getElementById("logoutBtn").style.display = "block";
-    } else {
-      // 로그인되지 않은 상태라면 로그인 버튼을 보여줍니다.
-      document.getElementById("loginBtn").style.display = "block";
-      document.getElementById("logoutBtn").style.display = "none";
-    }
+  const authInstance = gapi.auth2.getAuthInstance();
+  const isSignedIn = authInstance.isSignedIn.get();
+
+  if (isSignedIn) {
+    // 로그인된 상태라면 로그아웃 버튼을 보여줍니다.
+    document.getElementById("loginBtn").style.display = "none";
+    document.getElementById("logoutBtn").style.display = "block";
+  } else {
+    // 로그인되지 않은 상태라면 로그인 버튼을 보여줍니다.
+    document.getElementById("loginBtn").style.display = "block";
+    document.getElementById("logoutBtn").style.display = "none";
+  }
 }
-  
-  // signIn() 함수는 사용자를 로그인시키는 기능을 구현합니다.
+
 function signIn() {
-    const authInstance = gapi.auth2.getAuthInstance();
-    authInstance.signIn().then(() => {
-      updateLoginStatus();
-    });
+  const authInstance = gapi.auth2.getAuthInstance();
+  authInstance.signIn().then(() => {
+    updateLoginStatus();
+  });
 }
-  
-  // signOut() 함수는 사용자를 로그아웃시키는 기능을 구현합니다.
+
 function signOut() {
-    const authInstance = gapi.auth2.getAuthInstance();
-    authInstance.signOut().then(() => {
-      updateLoginStatus();
-    });
+  const authInstance = gapi.auth2.getAuthInstance();
+  authInstance.signOut().then(() => {
+    updateLoginStatus();
+  });
 }
 
 function setTodoListToSheet(task) {
   return gapi.client.sheets.spreadsheets.values
     .append({
       spreadsheetId: SPREADSHEET_ID,
-      range: "Sheet1!A:D", // A부터 D열까지 데이터를 추가합니다.
+      range: "Sheet1!A:A", // A열에만 데이터를 추가합니다.
       valueInputOption: "RAW", // 입력과 동일한 형식으로 값을 쓰려면 'RAW'를 사용합니다.
       insertDataOption: "INSERT_ROWS", // 필요한 경우 새 행을 삽입합니다.
       resource: {
@@ -101,6 +97,4 @@ function setTodoListToSheet(task) {
     });
 }
 
-function handleClientLoad() {
-    gapi.load("client:auth2", initClient);
-}
+gapi.load("client:auth2", initClient);
